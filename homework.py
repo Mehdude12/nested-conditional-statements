b13ecd7d0ff073c626818c82d0c02eb03ab121d2
Mehdude12 <mesembrydesigns@gmail.com>
2026-05-25 13:49:10 +0530

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,6 @@
+print("Welcome to Mr. raj's english class\n")
+age = int(input("Please enter your age: "))
+if age >= 10 and age <= 20:
+    print("You are applicable for the class🥳")
+else:
+    print("You are not applicable for this class😔")
